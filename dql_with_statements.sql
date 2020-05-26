de47d9ca07609a4f0d918331b5937c14fe304f10
Paulo Martins <paulofelipe0896@gmail.com>
2020-05-25 22:56:59 -0300

    Consulta com WITH STATEMENT e JOINS

---
+++ dql_with_statements.sql
@@ -0,0 +1,56 @@
+SELECT numero, nome FROM banco;
+SELECT banco_numero, numero, nome FROM agencia;
+
+WITH tbl_tmp_banco AS (
+	SELECT numero, nome
+	FROM banco
+)
+SELECT numero, nome
+FROM tbl_tmp_banco;
+
+WITH params AS (
+	SELECT 213 AS banco_numero
+), tbl_tmp_banco AS (
+	SELECT numero, nome
+	FROM banco
+	JOIN params ON params.banco_numero = banco.numero
+)
+SELECT numero, nome
+FROM tbl_tmp_banco;
+
+-- SUBSELECT
+SELECT banco.numero, banco.nome
+FROM banco
+JOIN (
+	SELECT 213 AS banco_numero
+) params ON params.banco_numero = banco.numero;
+
+-- Statement, não utilizado pelas aplicações
+WITH clientes_e_transacoes AS (
+	SELECT
+		cliente.nome AS cliente_nome,
+		tipo_transacao.nome AS tipo_transacao_nome,
+		cliente_transacoes.valor AS tipo_transacao_valor
+	FROM cliente_transacoes
+	JOIN cliente ON cliente.numero = cliente_transacoes.cliente_numero
+	JOIN tipo_transacao ON tipo_transacao.id_transacao = cliente_transacoes.tipo_transacao_id
+)
+-- Select final, utilizado pelas aplicações
+SELECT cliente_nome, tipo_transacao_nome, tipo_transacao_valor
+FROM clientes_e_transacoes;
+
+-- Statement, não utilizado pelas aplicações
+WITH clientes_e_transacoes AS (
+	SELECT
+		cliente.nome AS cliente_nome,
+		tipo_transacao.nome AS tipo_transacao_nome,
+		cliente_transacoes.valor AS tipo_transacao_valor,
+		banco.nome AS banco_nome
+	FROM cliente_transacoes
+	JOIN cliente ON cliente.numero = cliente_transacoes.cliente_numero
+	JOIN tipo_transacao ON tipo_transacao.id_transacao = cliente_transacoes.tipo_transacao_id
+	JOIN banco ON banco.numero = cliente_transacoes.banco_numero AND banco.nome ILIKE '%Bradesco%'
+)
+-- Select final, utilizado pelas aplicações
+SELECT cliente_nome, banco_nome, tipo_transacao_nome, tipo_transacao_valor
+FROM clientes_e_transacoes;
